fcc1cf68d9343527a08f31d9227d0ebab0da7750
Rob <brem_robert@hotmail.com>
2018-05-27 12:37:59 +0200

    [BUG] start with managment console to debug

--- start.sh
+++ start.sh
@@ -40,4 +40,4 @@ echo "=> Deploy application"
 mv ${JBOSS_HOME}/*.war ${JBOSS_HOME}/standalone/deployments/
 
 echo "=> Restarting WildFly"
-$JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -c $JBOSS_CONFIG
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c $JBOSS_CONFIG
