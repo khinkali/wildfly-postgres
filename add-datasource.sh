d409c34563cd1c97e1ce9ffa020cdaa1adb75a30
Rob <brem_robert@hotmail.com>
2018-05-27 13:15:58 +0200

    [BUG] correct typo in add-database

--- add-datasource.sh
+++ add-datasource.sh
@@ -3,7 +3,7 @@
 $JBOSS_HOME/bin/jboss-cli.sh --connect --user=${WILDFLY_USER} --password=${WILDFLY_PASSWORD} <<EOF
 batch
 deploy ${JBOSS_HOME}/postgresql-42.2.2.jar
-data-source add --jndi-name=${JNDI_NAME} --name=${DATASOURCE_NAME} --connection-url=jdbc:postgres://${DB_HOST}:${DB_PORT}/${DATABASE_NAME} --driver-name=postgresql-42.2.2.jar --user-name=${DATABASE_USER} --password=${DATABASE_PASSWORD}
+data-source add --jndi-name=${JNDI_NAME} --name=${DATASOURCE_NAME} --connection-url=jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DATABASE_NAME} --driver-name=postgresql-42.2.2.jar --user-name=${DATABASE_USER} --password=${DATABASE_PASSWORD}
 run-batch
 exit
 EOF
